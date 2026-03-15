f9aee68354aa2976ab2d3731d97f186d8fb3639f
Shuxin Wang <shuxinwang662@gmail.com>
2026-03-15 22:26:49 +0800

    Update main.rs

--- file_uploader_core/src/main.rs
+++ file_uploader_core/src/main.rs
@@ -7,9 +7,11 @@ use tracing::info;
 
 fn main() {
     // init_logging;
-    if let Err(e) = init_logging("temp_uploader.log") {
+    if let Err(e) = init_logging("") {
         eprintln!("Failed to initialize logging: {}", e);
         return;
+    } else {
+        info!("Logging initialized");
     }
 
     info!("Hello, world!")
