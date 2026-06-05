73fa79624777222cea2e09af06b080110cf236c5
A-BigTree <shuxinwang662@gmail.com>
2026-06-05 17:10:11 +0800

    Add AGENTS.md with project docs, update file_type_filter logging

--- file_uploader_core/src/plugins/file_type_filter.rs
+++ file_uploader_core/src/plugins/file_type_filter.rs
@@ -1,3 +1,4 @@
+use tracing::info;
 use file_uploader_sdk::models::ctx::{UploadInputCtx, UploadOutputCtx};
 use file_uploader_sdk::models::enums::OutputResultType;
 use file_uploader_sdk::models::interface::UploadPlugin;
@@ -10,6 +11,7 @@ impl UploadPlugin for FileTypeFilter {
     }
 
     fn execute(&self, ctx: &UploadInputCtx) -> UploadOutputCtx {
+        info!("file-type-filter: {}", serde_json::to_string(ctx).unwrap_or("input error".to_string()));
         UploadOutputCtx {
             result: OutputResultType::Success,
             message: "Success".to_string(),
